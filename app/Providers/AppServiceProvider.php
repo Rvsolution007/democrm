eb78cc8eb21ccf215aacb6f0df5c1b1b4ede18e2
RV Solution <rvsolution696@gmail.com>
2026-03-09 11:57:23 +0530

    Disable forceScheme https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        // if (config('app.env') === 'production') {
+        //     \Illuminate\Support\Facades\URL::forceScheme('https');
+        // }
 
         // Configure rate limiters
         RateLimiter::for('api', function (Request $request) {
