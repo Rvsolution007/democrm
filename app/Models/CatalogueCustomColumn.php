f8e39c9621ce11c35e7020611ad65f08459b4317
RV Solution <rvsolution696@gmail.com>
2026-03-25 20:16:10 +0530

    Remove SoftDeletes from CatalogueCustomColumn to enforce hard deletes

--- app/Models/CatalogueCustomColumn.php
+++ app/Models/CatalogueCustomColumn.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
+
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
@@ -11,7 +11,7 @@ use App\Models\Traits\BelongsToCompany;
 
 class CatalogueCustomColumn extends Model
 {
-    use SoftDeletes, BelongsToCompany;
+    use BelongsToCompany;
 
     protected $fillable = [
         'company_id',
